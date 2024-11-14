0bdc8adbf6e80938f857f30e9d37338eddb11859
DuongDucManh <manhmanhd0402@gmail.com>
2024-11-14 16:40:37 +0700

    inti

---
+++ Todo-List-Web/js/main.js
@@ -0,0 +1,117 @@
+import ToDoItem from "./todoItem";
+import ToDoList from "./todolist";
+import Test from "./test";
+
+const toDoList = new ToDoList();
+
+// launch app
+document.addEventListener("readystatechange", (event) => {
+    if(event.target.readyState = "complete"){
+        initApp();
+    }
+});
+
+const initApp = () => {
+    //add listener
+    const itemEntryForm = document.getElementById("itemEntryForm");
+    itemEntryForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        processSubmission();
+    })
+    //procedural
+    //load list objects
+    refreshThePage();
+}
+
+const refreshThePage = () => {
+    clearListDisplay();
+    renderList();
+    clearItemEntryField();
+    setFocusOnItemEntryField();
+}
+
+const clearListDisplay = () => {
+    const parentElement = document.getElementById("itemList");
+    deleteContents(parentElement);
+}
+
+const deleteContents = (parentElement) => {
+    let child = parentElement.lastElementChild;
+    while(child){
+        parentElement.removeChild(child);
+        child = parentElement.lastElementChild;
+    }
+}
+
+const renderList = () => {
+    const list = toDoList.getList();
+    list.forEach(item => {
+        buildListItem(item);
+    });
+}
+
+const buildListItem = (item) => {
+    const div = document.createElement("div");
+    div.className = "item";
+    const check = document.createElement("input");
+    check.type = "checkbox";
+    check.id = item.getId();
+    check.tabIndex = 0;
+    addClickListenerToCheckbox(check);
+    const label = document.createElement("label");
+    label.htmlFor = item.getId();
+    label.textContent = item.getItem();
+    div.appendChild(check);
+    div.appendChild(label);
+    const container = document.getElementById("listItems");
+    container.appendChild(div);
+}
+
+const addClickListenerToCheckbox = (checkbox) => {
+    checkbox.addEventListener("click", (event) => {
+        toDoList.removeItem(checkbox.id);
+        //TODO remove persistent data
+        setTimeout(() => {
+            refreshThePage();
+        }, 1000)
+    });
+}
+
+const clearItemEntryField = () => {
+    document.getElementById("newItem").value = "";
+}
+
+const setFocusOnItemEntryField = () => {
+    document.getElementById("newItem").focus();
+}
+
+const processSubmission = () => {
+    const newEntryText = getNewEntry();
+    if(!newEntryText.length) return;
+    const nextItemId = calcNextItemId();
+    const todoItem = createNewItem(nextItemId, newEntryText);
+    toDoList.addNewItem(todoItem);
+    //Todo update persistent data
+    refreshThePage();
+}
+
+const getNewEntry = () => {
+    return document.getElementById("newItem").value.trim();
+}
+
+const calcNextItemId = () => {
+    let nextItemId = 1;
+    const list = toDoList.getList();
+    if(list.length > 0){
+        nextItemId = list[list.length - 1].getId() + 1;
+    }
+
+    return nextItemId;
+}
+
+const createNewItem = (itemId, itemText) => {
+    const todo = new ToDoItem();
+    todo.setId(itemId);
+    todo.setItem(itemText);
+    return todo;
+}
---
+++ Todo-List-Web/js/todolist.js
@@ -0,0 +1,25 @@
+export default class ToDoList{
+    constructor() {
+        this._list = [];
+    }
+
+    getList() {
+        return this._list;
+    }
+    clearList(){
+        this._list = [];
+    }
+    addNewItem(itemObj){
+        this._list.push(itemObj);
+    }
+
+    removeItem(id){
+        const list = this._list;
+        for(let i = 0; i < list.length; i++){
+            if(list[i]._id == id){
+                list.splice(i, 1);
+                break;
+            }
+        }
+    }
+}
